7cb3fded63dab8166d38a845904672d5f8ecf915
Brian Xiao <brianxiao@v1040-wn-rt-a-57-32.campus-dynamic.uwaterloo.ca>
2024-07-26 19:58:28 -0400

    1004 fix

--- Two Pointer/1004. Max Consecutive Ones III/solution.cpp
+++ Two Pointer/1004. Max Consecutive Ones III/solution.cpp
@@ -19,25 +19,4 @@ public:
         }
         return retval;
     }
-};class Solution {
-public:
-    int longestOnes(vector<int>& nums, int k) {
-        int retval = 0;
-        int currMax = 0;
-        int kleft = k;
-        int i = 0;
-        for (int j = 0; j < nums.size(); j++) {
-            currMax++;
-            if (nums[j] == 0) kleft--;
-            while (kleft < 0) {
-                if (nums[i] == 0) {
-                    kleft++;
-                }
-                currMax--;
-                i++;
-            }
-            retval = max(retval, currMax);
-        }
-        return retval;
-    }
 };

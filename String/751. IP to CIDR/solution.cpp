d05372f341ab07cd4ebf5ab19484f9d0a35a7200
Brian Xiao <brianxiao@Brians-MBP-2.attlocal.net>
2024-08-21 22:52:12 -0700

    751

--- String/751. IP to CIDR/solution.cpp
+++ String/751. IP to CIDR/solution.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    string toCIDR(long binRep, int step) {
+    string toCIDR(unsigned int binRep, int step) {
         return to_string((binRep >> 24) & 255) + "." +
                to_string((binRep >> 16) & 255) + "." +
                to_string((binRep >> 8) & 255) + "." +
@@ -10,19 +10,20 @@ public:
 
     vector<string> ipToCIDR(string ip, int n) {
         vector<string> retval;
-        long binRep = 0;
+        unsigned int binRep = 0;
         int j = 0;
         for (int i = 0; i <= ip.size(); i++) {
             if (ip[i] == '.' || i == ip.size()) {
                 string temp = ip.substr(j, i - j);
                 binRep = (binRep << 8) + stoi(temp);
-                cout << temp << endl;
                 j = i + 1;
             }
         }
         while (n > 0) {
-            long step = binRep & -binRep;
-            while (step > n) step /= 2;
+            unsigned int step = binRep & -binRep;
+            if (step == 0 || step > n) {
+                step = 1 << (31 - __builtin_clz(n));
+            }
             retval.push_back(toCIDR(binRep, step));
             binRep += step;
             n -= step;
